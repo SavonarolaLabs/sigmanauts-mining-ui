5326a622a495afa343b39abfa7ab01531186b3e9
c8e4 <c8e4@proton.me>
2023-09-30 00:05:41 +0200

    Updates

---
+++ _app/immutable/chunks/paths.6e910634.js
@@ -0,0 +1,1 @@
+var s;const i=((s=globalThis.__sveltekit_1gkiruj)==null?void 0:s.base)??"/sigmanauts-mining-ui";var a;const t=((a=globalThis.__sveltekit_1gkiruj)==null?void 0:a.assets)??i;export{t as a,i as b};
